cfc7915b680860805ca85afaed519a996a091703
Carminelm <carminelm54@gmail.com>
2024-09-16 17:09:01 +0200

    add controller resource

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
+use Illuminate\Support\Str;
 
 class ComicSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $array_comics = config('comics');
+        foreach ($array_comics as $comic){
+            $new_comic = new Comic();
+            $new_comic->title =$comic('title');
+            $new_comic->description =$comic('description');
+            $new_comic->thumb =$comic('thumb');
+            $new_comic->price =$comic('price',4,2);
+            $new_comic->series =$comic('series');
+            $new_comic->sale_date =$comic('sale_date');
+            $new_comic->type =$comic('type');
+
+            $new_comic->save();
+
+            
+        }
     }
 }
